6e4b495fdf13eeb4fe04c9eded673439aff1f330
Shriphani Palakodety <shriphanip@gmail.com>
2015-11-15 01:25:30 -0800

    added example

--- vad.py
+++ vad.py
@@ -11,4 +11,11 @@ def free_vad(vad_inst):
     webrtc_vad.WebRtcVad_Free(vad_inst)
 
 def process_frame(vad_inst, fs, frame):
-    return webrtc_vad.processFrame(vad_inst, fs, frame)
+    return webrtc_vad.processFrame(vad_inst, fs, frame) > 0
+
+if __name__ == '__main__':
+
+    vad_inst = create_vad()
+
+    # Sampled at 8000hz, 160 frames of pure silence - should be 0
+    print 'Speech in silence?', process_frame(vad_inst, 8000, [0 for _ in range(160)])
